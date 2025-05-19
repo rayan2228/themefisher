722f1ea90ba666ab3527776322c0fe4242fb0f9c
rayan2228 <rayanh.dev@gmail.com>
2025-05-19 17:02:54 +0600

    feat: Add Navbar component with navigation links and icons; remove unused SVG icons

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,9 +4,11 @@ import rightShape from "@/app/assets/images/right-shape.png";
 import SocialIcons from "@/components/SocialIcons";
 import Image from "next/image";
 import Introduction from "./Introduction";
+import Navbar from "./Navbar";
 const Hero = () => {
     return (
         <section className=" mt-27">
+            <Navbar/>
             <Image src={leftShape} alt="bg" className="object-cover fixed bottom-50 left-0" />
             <Image src={rightShape} alt="bg" className="object-cover fixed bottom-50 right-0" />
             <div className="px-23">
---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,50 @@
+import aboutIcon from "@/app/assets/icons/about.svg"
+import blogIcon from "@/app/assets/icons/blog.svg"
+import contactIcon from "@/app/assets/icons/contact.svg"
+import homeIcon from "@/app/assets/icons/home.svg"
+import portfolioIcon from "@/app/assets/icons/portfolio.svg"
+import Image from "next/image"
+import Link from "next/link"
+const navbarDetails = [
+    {
+        name: "Home",
+        url: "/",
+        icon: homeIcon
+    },
+    {
+        name: "About",
+        url: "#about",
+        icon: aboutIcon
+    },
+    {
+        name: "Portfolio",
+        url: "#portfolio",
+        icon: portfolioIcon
+    },
+    {
+        name: "Blog",
+        url: "#blog",
+        icon: blogIcon
+    },
+    {
+        name: "Contact",
+        url: "#contact",
+        icon: contactIcon
+    }
+]
+
+const Navbar = () => {
+    return (
+        <nav className="flex flex-col items-center gap-10 fixed z-30 top-50 left-8 ">
+            {
+                navbarDetails.map((item) => {
+                    return (
+                        <Link key={item.name} href={item.url} className="text-white hover:text-primary"><Image src={item.icon} alt={item.name} /></Link>
+                    )
+                })
+            }
+        </nav>
+    )
+}
+
+export default Navbar
